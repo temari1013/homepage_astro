506d2aceed40d8b886880b181cdfa4feb30f39d5
temari1013 <smile21512211@icloud.com>
2025-04-18 18:46:23 +0900

    ESlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,17 @@
-// eslint.config.js
-import js from '@eslint/js'
-import { defineConfig } from 'eslint/config'
+
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
 
 export default defineConfig([
-  {
-    files: ['**/*.js'],
-    plugins: {
-      js
-    },
-    extends: ['js/recommended'],
-    rules: {
-      'no-unused-vars': 'warn'
-    }
-  }
-])
+	{
+		files: ["**/*.js"],
+		plugins: {
+			js,
+		},
+		extends: ["js/recommended"],
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "warn",
+		},
+	},
+]);
